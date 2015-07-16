ef161628d876be5826eacc04abd609ead559db6c
lhannest <lancehannestad@gmail.com>
2015-07-15 18:58:56 -0700

    Initial commit of NeuralNetwork, Node and Genome.

---
+++ Evolution/src/neuralnetwork/NeuralNetwork.java
@@ -0,0 +1,23 @@
+package neuralnetwork;
+
+public class NeuralNetwork {
+	public NeuralNetwork(int inputSize, int hiddenSize, int outputSize) {
+		throw new RuntimeException();
+	}
+	
+	public void setInput(int i) {
+		throw new RuntimeException();
+	}
+	
+	public double getOutput(int i) {
+		throw new RuntimeException();
+	}
+	
+	public int inputSize() {
+		throw new RuntimeException();
+	}
+	
+	public int outputSize() {
+		throw new RuntimeException();
+	}
+}
